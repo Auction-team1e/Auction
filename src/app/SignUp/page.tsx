476ffc6b0f2d1b7c6b601b44626c5506593c9e4b
Badral Tumursukh <23LP6834@G401-CC0100.local>
2024-04-17 12:36:39 +0800

    from class

--- src/app/SignUp/page.tsx
+++ src/app/SignUp/page.tsx
@@ -1,3 +1,21 @@
-export default function Login() {
-  return <>Login page</>;
+import { Stack, Typography } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+
+export default function SignUp() {
+  return (
+    <Stack
+      alignItems={`center`}
+      width={424}
+      border={`1px solid black`}
+      height={666.5}
+      py={1.6}
+    >
+      <CloseIcon
+        sx={{ position: `relative`, left: 170, fontSize: 30, fontWeight: 300 }}
+      />
+      <Typography fontSize={24} fontWeight={500}>
+        Sign up
+      </Typography>
+    </Stack>
+  );
 }
