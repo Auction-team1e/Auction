7e7ba213709419de82ebebd7ee80365f56dea465
Dashnorov <151482618+dashaa1204@users.noreply.github.com>
2024-04-17 12:09:45 +0800

    class

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,22 @@
 "useclient";
 
+const titles = [
+  "About",
+  "Contact",
+  "Careers",
+  "Help & FAQ",
+  "Terms",
+  "Privacy",
+];
+
 import { Stack } from "@mui/material";
 
 export const Footer = () => {
-  return <Stack>footer</Stack>;
+  return (
+    <Stack bgcolor={"black"} color={"white"}>
+      <Stack px={"96px"} height={"540px"}>
+        <Stack gap={"20px"}></Stack>
+      </Stack>
+    </Stack>
+  );
 };
