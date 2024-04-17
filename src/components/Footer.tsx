c2400024ee9982fe73743624445880308d0e42e9
Dashnorov <151482618+dashaa1204@users.noreply.github.com>
2024-04-17 12:39:40 +0800

    footer bg

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,21 +1,49 @@
 "useclient";
 
-const titles = [
-  "About",
-  "Contact",
-  "Careers",
-  "Help & FAQ",
-  "Terms",
-  "Privacy",
+const footer = [
+  {
+    title: "JamesEdition",
+    subTitles: [
+      "About",
+      "Contact",
+      "Careers",
+      "Help & FAQ",
+      "Terms",
+      "Privacy",
+    ],
+  },
+  {
+    title: "Brands",
+    subTitles: [
+      "Mercedes Benz",
+      "BMW",
+      "Buggatti",
+      "Lamborghini",
+      "Ferrari",
+      "Pagani",
+      "Brabus",
+      "Bentley",
+      "Audi",
+      "McLaren",
+      "Porsche",
+    ],
+  },
 ];
 
-import { Stack } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 
 export const Footer = () => {
   return (
     <Stack bgcolor={"black"} color={"white"}>
-      <Stack px={"96px"} height={"540px"}>
-        <Stack gap={"20px"}></Stack>
+      <Stack px={"96px"} height={"540px"} direction={"row"}>
+        {footer.map((a, index) => {
+          return (
+            <Stack gap={"20px"} height={"full"} key={index}>
+              <Typography>{a.title}</Typography>
+              <Stack bgcolor={"blue"}>{a.subTitles}</Stack>
+            </Stack>
+          );
+        })}
       </Stack>
     </Stack>
   );
