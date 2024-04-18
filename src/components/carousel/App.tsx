52698d84c173f560c115e455a1ca105da4f35274
temuujin044 <temuujinbatdorj@gmail.com>
2024-04-18 10:55:15 +0800

    12

---
+++ src/components/carousel/App.tsx
@@ -0,0 +1,10 @@
+import { Stack } from "@mui/material";
+import { Carousel } from "./Carousel";
+
+export const App = () => {
+  return (
+    <Stack width={"100%"}>
+      <Carousel />
+    </Stack>
+  );
+};
