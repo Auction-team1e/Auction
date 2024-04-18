357147679f636425d7249b261077cbd8a28ea35e
tsogbadrakh baatar <23LP4804@G401-CC0101.local>
2024-04-18 12:35:31 +0800

    4.18

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,9 +7,12 @@ import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 
-import SearchSharpIcon from "@mui/icons-material/SearchSharp";
+import TextField from "@mui/material/TextField";
 
-export default function Header() {
+import SearchSharpIcon from "@mui/icons-material/SearchSharp";
+import Popover from "@mui/material/Popover";
+
+export function Header() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" sx={{ bgcolor: "transparent" }}>
@@ -25,24 +28,9 @@ export default function Header() {
             JamesAuction
           </Typography>
           <SearchSharpIcon />
-          <Button
-            disableElevation={true}
-            variant="contained"
-            sx={{
-              textTransform: "none",
-              color: "#717171",
-              fontSize: "16",
-              background: "#fff",
-              borderRadius: "100px",
-              "&:hover": { background: "#fff" },
-              width: 540,
-              height: 38,
-            }}
-          >
-            Search Cars
-          </Button>
-
-          <Button sx={{ color: "black", borderRadius: "1" }}>Log in</Button>
+          <TextField variant="outlined" label="Search Car"></TextField>
+          <Button sx={{ color: "black" }}>Sell with us</Button>
+          <Button sx={{ color: "black", borderRadius: "30%" }}>Log in</Button>
         </Toolbar>
       </AppBar>
     </Box>
