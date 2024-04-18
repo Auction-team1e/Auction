6381b4e4e2e9e7df8ca3999118b4303567b7d1c6
Dashnorov <151482618+dashaa1204@users.noreply.github.com>
2024-04-18 10:46:15 +0800

    footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-"useclient";
+"use client";
 
 const footer = [
   {
@@ -28,22 +28,66 @@ const footer = [
       "Porsche",
     ],
   },
+  {
+    title: "For Business",
+    subTitles: ["Sell With Us", "Partner", "Linking"],
+  },
 ];
 
-import { Box, Stack, Typography } from "@mui/material";
+const language = ["English", "Mongolian"];
+const currency = ["USD", "MNT"];
+
+import { Box, Select, Stack, Typography } from "@mui/material";
+import { FooterSelect } from "./FooterSelect";
 
 export const Footer = () => {
   return (
-    <Stack bgcolor={"black"} color={"white"}>
-      <Stack px={"96px"} height={"540px"} direction={"row"}>
+    <Stack
+      px={"96px"}
+      direction={"row"}
+      bgcolor={"black"}
+      color={"white"}
+      pt={"48px"}
+      justifyContent={"space-between"}
+    >
+      <Stack direction={"row"}>
         {footer.map((a, index) => {
           return (
-            <Stack gap={"20px"} height={"full"} key={index}>
-              <Typography>{a.title}</Typography>
-              <Stack bgcolor={"blue"}>{a.subTitles}</Stack>
+            <Stack width={"200px"} height={"700px"} key={index} gap={"16px"}>
+              <Typography
+                fontSize={"12px"}
+                fontWeight={600}
+                textTransform={"uppercase"}
+                color={"#ADADAD"}
+              >
+                {a.title}
+              </Typography>
+              <Stack gap={"20px"}>
+                {a.subTitles.map((b) => {
+                  return (
+                    <Typography fontWeight={400} fontSize={"14px"}>
+                      {b}
+                    </Typography>
+                  );
+                })}
+              </Stack>
             </Stack>
           );
         })}
+      </Stack>
+      <Stack width={"200px"} gap={"16px"}>
+        <Typography
+          fontSize={"12px"}
+          fontWeight={600}
+          textTransform={"uppercase"}
+          color={"#ADADAD"}
+        >
+          Settings
+        </Typography>
+        <Stack>
+          <FooterSelect data={language} />
+          <FooterSelect data={currency} />
+        </Stack>
       </Stack>
     </Stack>
   );
