92a91e173dfca947d57387f0a6599dfce5e5bab3
Badral Tumursukh <23LP6834@G401-CC0100.local>
2024-04-23 11:27:24 +0800

    car add modal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,4 @@
-import { CarAddModal } from "@/components/CarAddModal/CarAddModal";
-import CarProvider from "./context/CarContext";
-
 export default function Home() {
-  return (
-    <CarProvider>
-      <CarAddModal />
-    </CarProvider>
-  );
+  return;
 }
 //ads
