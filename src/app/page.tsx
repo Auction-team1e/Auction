31072f506380a02a97457f41b47386420ca01eff
Dashnorov <151482618+dashaa1204@users.noreply.github.com>
2024-04-23 09:15:44 +0800

    class

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,4 @@
-import { FeaturedCard } from "@/components/FeaturedCard/FeaturedCard";
-import { Stack } from "@mui/material";
-
 export default function Home() {
-  return (
-    <Stack>
-      <FeaturedCard />
-    </Stack>
-  );
+  return;
 }
+//ads
