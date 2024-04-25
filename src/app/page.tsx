dd86ef02f0949bafc89347a898566cbd29be72c9
Badral Tumursukh <23LP6834@G401-CC0100.local>
2024-04-25 08:53:59 +0800

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Carousel } from "@/components/CarouselComp";
+import { PopMakes } from "@/components/PopMakes";
 import { Stack } from "@mui/material";
 
 export default function Home() {
   return (
-    <Stack>
+    <Stack gap={8}>
       <Carousel />
+      <PopMakes />
     </Stack>
   );
 }
