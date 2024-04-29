4e68650518664f0217d4303fdb1c0e511b461684
Badral Tumursukh <23LP6834@G401-CC0100.local>
2024-04-29 09:50:17 +0800

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,17 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
 import { Footer } from "@/components/Footer/Footer";
 import CarProvider from "@/context/DataContext";
 import { Header } from "@/components/Header/Header";
 import "react-toastify/dist/ReactToastify.css";
+import { MainLogo } from "@/svgs/TabLogo";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Luxury Car Auction",
+  icons: `/${MainLogo}`,
 };
 
 const RootLayout = ({
