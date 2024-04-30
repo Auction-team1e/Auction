3f85ccc40240fc55ce66b3de271f1ca562f1e8f4
temuujin044 <temuujinbatdorj@gmail.com>
2024-04-30 11:01:41 +0800

    3

---
+++ src/components/AdminHeader.tsx
@@ -0,0 +1,34 @@
+import { Bell, Pinecone, Profile } from "@/svgs";
+import { ButtonBase, Stack, Typography } from "@mui/material";
+
+export const AdminHeader = () => {
+  return (
+    <Stack
+      height={"45px"}
+      alignItems={"center"}
+      justifyContent={"center"}
+      bgcolor={"black"}
+      m={"200px 0px 200px 0px"}
+      width={"100vw"}
+    >
+      <Stack
+        justifyContent={"space-between"}
+        direction={"row"}
+        width={"1720px"}
+      >
+        <Pinecone />
+        <Stack gap={"12px"} direction={"row"}>
+          <ButtonBase>
+            <Bell />
+          </ButtonBase>
+          <ButtonBase sx={{ gap: "8px" }}>
+            <Profile />
+            <Typography fontSize={"14px"} fontWeight={"400"} color={"white"}>
+              Username
+            </Typography>
+          </ButtonBase>
+        </Stack>
+      </Stack>
+    </Stack>
+  );
+};
