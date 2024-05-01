2ccf607897339468beaad43982714a54451ef5a8
Dashnorov <151482618+dashaa1204@users.noreply.github.com>
2024-05-01 09:31:52 +0800

    fix

--- src/app/carInfo/page.tsx
+++ src/app/carInfo/page.tsx
@@ -1,5 +1,5 @@
 import { CarDetail } from "@/components/CarDetailPage/CarDetail";
-// import { CarInfo } from "@/components/CarDetailPage/CarInfo";
+import { CarInfo } from "@/components/CarDetailPage/CarInfo";
 import { Contact } from "@/components/CarDetailPage/Contact";
 import { Stack } from "@mui/material";
 
@@ -8,7 +8,7 @@ const Page = () => {
     <Stack pt={"115px"} width={"100%"} alignItems={"center"}>
       <Stack>
         <CarDetail />
-        {/* <CarInfo /> */}
+        <CarInfo />
         <Contact />
       </Stack>
     </Stack>
