5d8ce5f77029a3f67fcdf91175672167ae39d62f
tsogbadrakh baatar <tsogoobaatar650@gmail.com>
2024-05-01 11:45:13 +0800

    5-1

---
+++ src/app/sellWithUs/page.tsx
@@ -0,0 +1,17 @@
+import { Stack, Typography } from "@mui/material";
+
+const Page = () => {
+  return (
+    <Stack pt={"115px"} width={"100%"}>
+      <Typography fontSize={"46px"}>
+        Select your preferred listing category
+      </Typography>
+      <Typography fontSize={"14px"} width={"430px"}>
+        Select your category for a personalised experience. Your choice ensures
+        your offerings stand out in our exclusive marketplace. Enhance your
+        online presence with JamesEdition.
+      </Typography>
+    </Stack>
+  );
+};
+export default Page;
