e955d832cede92be5b534f39d0822f2909f9f87a
Badral Tumursukh <23LP6834@G401-CC0100.local>
2024-05-09 12:47:30 +0800

    fix

--- src/app/(noHeaderAndFooter)/carInfo/page.tsx
+++ src/app/(noHeaderAndFooter)/carInfo/page.tsx
@@ -1,5 +1,5 @@
-import { CarDetail } from "@/components/CarDetailPage/CarDetail";
-import { CarInfo } from "@/components/CarDetailPage/CarInfo";
+// import { CarDetail } from "@/components/CarDetailPage/CarDetail";
+// import { CarInfo } from "@/components/CarDetailPage/CarInfo";
 import { Contact } from "@/components/CarDetailPage/Contact";
 import { Stack } from "@mui/material";
 
@@ -7,8 +7,8 @@ const Page = () => {
   return (
     <Stack width={"100%"} alignItems={"center"}>
       <Stack>
-        <CarDetail />
-        <CarInfo />
+        {/* <CarDetail />
+        <CarInfo /> */}
         <Contact />
       </Stack>
     </Stack>
